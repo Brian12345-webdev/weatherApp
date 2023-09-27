8f332c6603674ce1fa849037a3fb53be9d5fd516
Brian12345-webdev <132722257+Brian12345-webdev@users.noreply.github.com>
2023-09-27 17:32:40 -0500

    update to divs in html file, start functions for retrieving weather data in js file

--- assets/index.js
+++ assets/index.js
@@ -1,11 +1,25 @@
-var searchTable = document.querySelector('.search-table');
+// var searchTable = document.querySelector('.search-table');
+// 
+function getCurrentWeather(data) {
+    console.log(data);
+}
+//     fetch("https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=c5d423c4815b48e87a0ce37757048cf1", {
+// })
+//     .then(function (response) {
+//         return response.json;
+//     })
 
-fetch("https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={c5d423c4815b48e87a0ce37757048cf1}", {
-})
+//     .then(function (data) {
+
+//     })
+
+  function getGeoCode(){
+    fetch("http://api.openweathermap.org/geo/1.0/direct?q=Shreveport&appid=c5d423c4815b48e87a0ce37757048cf1")
     .then(function (response) {
-        return response.json;
-    })
+        return response.json();
+  })
+  .then(function (data) {
+    getCurrentWeather(data);});
+  }
 
-    .then(function (data) {
-        console.log(data);
-    })
+  getGeoCode() //
