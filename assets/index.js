2fe77eac7ef67261b4ddd6df9590f9abba7e6a9e
Brian12345-webdev <132722257+Brian12345-webdev@users.noreply.github.com>
2023-09-28 18:43:02 -0500

    updates to html cards, js fetch and new variables

--- assets/index.js
+++ assets/index.js
@@ -1,24 +1,33 @@
 var geoCode = getGeoCode
-// 
+var fiveDayForecast = document.querySelector('.five-day')
+var cityButton = document.querySelector('#btn btn-primary')
 function getCurrentWeather(data) {
-    console.log(data);
-    //fetch("https://api.openweathermap.org/data/2.5/forecast?q=' + geoCode + '&appid=c5d423c4815b48e87a0ce37757048cf1")
-    //     .then(function (response) {
-    //         return response.json;
-    //     })
+var lat = data[0].lat
+var lon = data[0].lon
+
+  fetch("https://api.openweathermap.org/data/2.5/forecast?lat=lat&lon=lon&appid=c5d423c4815b48e87a0ce37757048cf1")
+  
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      for (var i = 0; i < data.length; i++) {
+        cityButton = document.createElement('button');
+        console.log(cityButton)
+}})
 }
 
-//     .then(function (data) {
-
-//     })
-
-  function getGeoCode(){
-    fetch("http://api.openweathermap.org/geo/1.0/direct?q=Shreveport&appid=c5d423c4815b48e87a0ce37757048cf1")
+function getGeoCode() {
+  fetch("http://api.openweathermap.org/geo/1.0/direct?q=Shreveport&appid=c5d423c4815b48e87a0ce37757048cf1")
     .then(function (response) {
-        return response.json();
-  })
-  .then(function (data) {
-    getCurrentWeather(data);});
-  }
+      return response.json();
+    })
+    .then(function (data) {
+      getCurrentWeather(data);
+    });
+}
 
-  getGeoCode() //
+getGeoCode()
+
+
