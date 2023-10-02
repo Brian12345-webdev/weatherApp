78e68bddc14c4c029e1263fdf2126f1645ec1b46
Brian12345-webdev <132722257+Brian12345-webdev@users.noreply.github.com>
2023-10-02 16:35:09 -0500

    update to js

--- assets/index.js
+++ assets/index.js
@@ -1,22 +1,25 @@
 var geoCode = getGeoCode
 var fiveDayForecast = document.querySelector('.five-day')
 var cityButton = document.querySelector('#btn btn-primary')
-function getCurrentWeather(data) {
-var lat = data[0].lat
-var lon = data[0].lon
 
-  fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=c5d423c4815b48e87a0ce37757048cf1`)
+
+function getCurrentWeather(data) {
+  console.log(data)
   
+
+  fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${data}&appid=c5d423c4815b48e87a0ce37757048cf1`)
+
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
       console.log(data);
-      for (var i = 0; i < data.length; i++) {
-        cityButton = document.createElement('button');
-        console.log(cityButton)
-}})
+      fiveDayForecastCards(data);
+    })
+    
 }
+
+
 
 function getGeoCode() {
   fetch("http://api.openweathermap.org/geo/1.0/direct?q=Shreveport&appid=c5d423c4815b48e87a0ce37757048cf1")
@@ -28,20 +31,38 @@ function getGeoCode() {
     });
 }
 
-function search(){
+
+
+function search() {
   this.getCurrentWeather(document.querySelector("#search-bar").value);
   console.log(value);
 };
 
 document.querySelector("#search-bar").addEventListener("click", function () {
-  getCurrentWeather.search();
+  console.log(document.querySelector("#text-input").value)
+  document.querySelector("#text-input").value
+  getCurrentWeather(document.querySelector("#text-input").value);
 });
 
-document.querySelector("#search-bar").addEventListener("keyup", function(event) {
+document.querySelector("#search-bar").addEventListener("keydown", function (event) {
   if (event.key == "Enter") {
     getCurrentWeather.search();
   }
+
+  document.querySelector(".card").addEventListener("click", function (event) {
+    getCurrentWeather.search
+  });
+
+  
+
 });
+
+function fiveDayForecastCards (forecast) {
+  console.log(forecast);
+   for (var i = 0; i < 5; i++) {
+    getCurrentWeather.applyWeather('#card');
+    }
+}
 
 getGeoCode()
 
