cb5009ab5ee1ee2add00797aa79fc83f683479f2
Brian12345-webdev <132722257+Brian12345-webdev@users.noreply.github.com>
2023-10-02 19:06:38 -0500

    update to js file

--- assets/index.js
+++ assets/index.js
@@ -2,10 +2,8 @@ var geoCode = getGeoCode
 var fiveDayForecast = document.querySelector('.five-day')
 var cityButton = document.querySelector('#btn btn-primary')
 
-
-function getCurrentWeather(data) {
+function getFiveDayWeather(data) {
   console.log(data)
-  
 
   fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${data}&appid=c5d423c4815b48e87a0ce37757048cf1`)
 
@@ -16,32 +14,43 @@ function getCurrentWeather(data) {
       console.log(data);
       fiveDayForecastCards(data);
     })
-    
 }
 
+function currentWeather(data){
+  var lat = data[0].lat
+  var lon = data[0].lon
+  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=c5d423c4815b48e87a0ce37757048cf1&units=imperial`)
 
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function(data) {
+    console.log(data);
+  })
+}
 
-function getGeoCode() {
-  fetch("http://api.openweathermap.org/geo/1.0/direct?q=Shreveport&appid=c5d423c4815b48e87a0ce37757048cf1")
+function getGeoCode(cityName) {
+  fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=c5d423c4815b48e87a0ce37757048cf1`)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      getCurrentWeather(data);
+      console.log(data);
+      getFiveDayWeather(cityName);
+      currentWeather(data)
     });
 }
 
-
-
 function search() {
-  this.getCurrentWeather(document.querySelector("#search-bar").value);
+  this.getFiveDayWeather(document.querySelector("#search-bar").value);
   console.log(value);
 };
 
 document.querySelector("#search-bar").addEventListener("click", function () {
   console.log(document.querySelector("#text-input").value)
-  document.querySelector("#text-input").value
-  getCurrentWeather(document.querySelector("#text-input").value);
+  document.querySelector("#text-input").value;
+  getFiveDayWeather(document.querySelector("#text-input").value);
+  getGeoCode(document.querySelector("#text-input").value);
 });
 
 document.querySelector("#search-bar").addEventListener("keydown", function (event) {
@@ -53,17 +62,17 @@ document.querySelector("#search-bar").addEventListener("keydown", function (even
     getCurrentWeather.search
   });
 
-  
-
 });
 
-function fiveDayForecastCards (forecast) {
-  console.log(forecast);
-   for (var i = 0; i < 5; i++) {
-    getCurrentWeather.applyWeather('#card');
-    }
+function fiveDayForecastCards () {
+  //  for (var i = 0; i < 5; i++) {
+  //   document.getElementById('five-day').textContent = 
+  //   }
 }
 
-getGeoCode()
+// function currentCity () {
+ // var currentCity = document.createElement("#current-city");
+
+
 
 
