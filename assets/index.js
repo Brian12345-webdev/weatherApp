c1a351424f6ef2295fbb40c0bd1b87d3d859e22b
Brian12345-webdev <132722257+Brian12345-webdev@users.noreply.github.com>
2023-10-02 22:04:22 -0500

    update to js to append data to document

--- assets/index.js
+++ assets/index.js
@@ -1,9 +1,8 @@
 var geoCode = getGeoCode
-var fiveDayForecast = document.querySelector('.five-day')
-var cityButton = document.querySelector('#btn btn-primary')
+var fiveDayForecastEl = document.querySelector('#five-day');
+var cityButton = document.querySelector('.btn btn-primary');
 
 function getFiveDayWeather(data) {
-  console.log(data)
 
   fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${data}&appid=c5d423c4815b48e87a0ce37757048cf1`)
 
@@ -12,21 +11,21 @@ function getFiveDayWeather(data) {
     })
     .then(function (data) {
       console.log(data);
-      fiveDayForecastCards(data);
+      displayFiveDayForecast(data);
     })
 }
 
-function currentWeather(data){
+function currentWeather(data) {
   var lat = data[0].lat
   var lon = data[0].lon
   fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=c5d423c4815b48e87a0ce37757048cf1&units=imperial`)
 
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function(data) {
-    console.log(data);
-  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    })
 }
 
 function getGeoCode(cityName) {
@@ -47,7 +46,6 @@ function search() {
 };
 
 document.querySelector("#search-bar").addEventListener("click", function () {
-  console.log(document.querySelector("#text-input").value)
   document.querySelector("#text-input").value;
   getFiveDayWeather(document.querySelector("#text-input").value);
   getGeoCode(document.querySelector("#text-input").value);
@@ -64,15 +62,23 @@ document.querySelector("#search-bar").addEventListener("keydown", function (even
 
 });
 
-function fiveDayForecastCards () {
-  //  for (var i = 0; i < 5; i++) {
-  //   var fiveDay = document.createElement('#five-day')
-  //   getFiveDayWeather.append(fiveDay);
-  //   }
+function displayFiveDayForecast(forecast) {
+  fiveDayForecastEl.textContent = forecast
+  for (var i = 0; i < 5; i++) {
+
+    var weather = document.createElement("div");
+    weather.classList = 'list-item flex-row justify-space-between align center';
+
+    var fiveDays = document.createElement("span");
+
+    weather.appendChild(fiveDays);
+
+    fiveDayForecastEl.appendChild(weather);
+  }
 }
 
 // function currentCity () {
- // var currentCity = document.createElement("#current-city");
+// var currentCity = document.createElement("#current-city");
 
 
 
